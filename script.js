28fa9e910451373d5739036157bd16bb28c0458c
deepak-dev1997 <108848941+deepak-dev1997@users.noreply.github.com>
2023-05-15 23:58:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+const input = document.getElementById("evaluatedText");
+const ouput= document.getElementById("letterCount");
+
+
+function updateOutput(){
+    let count = input.value.length; 
+    ouput.innerText=count;
+}
+
+input.addEventListener('keyup',updateOutput);
